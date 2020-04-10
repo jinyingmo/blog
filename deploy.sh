31b33826512846c92d3d1df1d63509b4038fddb7
jinyingmo <jinyingmo@maoyan.com>
2020-04-10 19:42:46 +0800

    fix: update deploy.js

--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,5 @@ set -e
 # 生成静态文件
 npm run build
 
-# 进入生成的文件夹
-cd docs/.vuepress/dist
-
-git init
-git add -A
-git commit -m 'deploy'
-
-# 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:dwanda/dwanda.github.io.git master
-
-cd -
+# 执行部署
+node deploy.js
